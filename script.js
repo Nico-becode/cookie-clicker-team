c13a97035fc3634b26249a5b890d5123365931a3
Massimo Tascone <tascone.massimo@gmail.com>
2019-09-24 12:12:16 +0200

    step 11 done

--- script.js
+++ script.js
@@ -5,49 +5,95 @@
     let multiplier = 1;
     let price_multiplier = 20;
     let autoclick = 0;
+    let price_autoclick = 100;
+    let seconds = 30;
+    let boost = 1;
+    let price_boost = 150;
+
 
     const multiplier_augment = 2;
-    const price_multiplier_augment = 3;
+    const price_augment = 3;
     const speed = 1000;
 
     let timer_autoclick = setInterval(change_score, speed, autoclick);
 
+    function timer(){
+        seconds--;
+        if(seconds > 0){
+            setTimeout(timer, speed);
+        }
+        else {
+            seconds = 30;
+            document.getElementById("boost").disabled = false;
+            boost = 1;
+
+        }
+        document.getElementById("boost").innerHTML = `Boost : ${seconds} left`;
+
+    }
+
 
     function change_score(value){
-        cookie_counter += value;
+        if (value >= 0){
+            cookie_counter += (value*boost);
+        }
+        else {
+            cookie_counter += value;
+        }
         document.getElementById("score").innerText = cookie_counter;
     }
-    
+    /* Click button */
     document.getElementById("cookie").addEventListener("click", () => {
         change_score(multiplier);
     });
-
+    /* Multiplier button */
     document.getElementById("multiplier").addEventListener("click", () => {
         
         if (price_multiplier <= cookie_counter) {
             change_score(-price_multiplier);
-            price_multiplier *= price_multiplier_augment;
+            price_multiplier *= price_augment;
             document.getElementById("price_multiplier").innerHTML = `Prix: ${price_multiplier} cookies`;
             multiplier *= multiplier_augment;
+            document.getElementById("multiplier").innerHTML= `Multiplier x ${multiplier} `;
         }
         else {
             console.log("tu peux pas");
         }
-        document.getElementById("multiplier").innerHTML= `x ${multiplier}`;
-    });
 
+    });
+    /* Autoclick*/
     document.getElementById("autoclick").addEventListener("click", () => {
-        clearInterval(timer_autoclick);
-        autoclick++;
-        timer_autoclick = setInterval(change_score, speed, autoclick);
+ 
+        if (price_autoclick <= cookie_counter) {
+            change_score(-price_autoclick);
+            clearInterval(timer_autoclick);
+            autoclick++;
+            timer_autoclick = setInterval(change_score, speed, autoclick);
+            price_autoclick *= price_augment;
+            document.getElementById("autoclick").innerHTML = `${autoclick} autoclick`;
+            document.getElementById("price_auto").innerHTML = `Prix : ${price_autoclick} cookies`;
+        }
         
-        document.getElementById("autoclick").innerHTML = `${autoclick} autoclick`;
-    });
 
+    });
+    /* Boost button */
     document.getElementById("boost").addEventListener("click", () => {
 
+        if (cookie_counter >= price_boost){
+            document.getElementById("boost").disabled = true;
+            change_score(-price_boost);
+            boost = 3;
+            setTimeout(timer, speed);
+            price_boost*= price_augment;
+        }
+        
+
+
+
 
     });
+
+
 
 
 })();
